32ddc971741ab1c5dbb177e0b43a0a84b5d95da4
张新永 <xinyong.zhang1@baozun.com>
2019-10-17 10:27:24 +0800

    feat: tslint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,7 +6,7 @@ export class UserController {
     @Render('default/user') // 渲染模版引擎
     index() {
         return {
-            name: 'zhangsan'
+            name: 'zhangsan',
         };
     }
     // @Query 装饰器 获取url？后面的参数
@@ -22,11 +22,12 @@ export class UserController {
     // @Body 获取传值
     @Post('create')
     create(@Body() body, @Response() res) {
+        // tslint:disable-next-line: no-console
         console.log(body);
         res.redirect('/user');
     }
     // 动态路由
-    @Get(":id")
+    @Get(':id')
     getId(@Param() param) {
         return param;
     }
