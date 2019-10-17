7f0183ea1ceabe420c84e691dcd4df5135733683
张新永 <xinyong.zhang1@baozun.com>
2019-10-17 17:45:32 +0800

    feat: Multer 是用于处理 multipart/form-data

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Get, Param, Post, Query, Render, Req, Res } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, Render, Req, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { Request, Response } from 'express';
 @Controller('user')
 export class UserController {
     @Get()
-    @Render('default/user') // 渲染模版引擎
+    @Render('default/upload') // 渲染模版引擎
     index() {
         return {
             name: 'zhangsan',
@@ -31,8 +32,27 @@ export class UserController {
         // res.redirect('/user');
     }
     // 动态路由
-    @Get(':id')
+    @Get(':id/:name')
     getId(@Param() param) {
         return param;
+    }
+    // post form-data  Multer 是用于处理 multipart/form-data 的中间件 ，主要用于上传文件。
+    // Multer 不会处理任何不是 multipart（multipart/form-data）的表单。此外，这个包不适用于 FastifyAdapter。
+    @Post('postFormData')
+    @UseInterceptors(FileInterceptor('file'))
+    postFormData(@Body() body) {
+        // 获取不是上传文件，但是form-data形式的
+        return {
+            body: { ...body },
+            susccess: 200,
+        };
+    }
+    // 文件上传
+    @Post('upload')
+    @UseInterceptors(FileInterceptor('file'))
+    uploadFile(@UploadedFile() file) { // 注意字段
+        return {
+            fileList: file,
+        };
     }
 }
